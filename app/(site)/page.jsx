c14275c6e000aa96891d2cc07a9c6982d03ce96a
Mark Slorach <hello@markslorach.com>
2024-02-27 20:46:45 +0000

    data fetching from sanity for homepage cards

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,7 +1,51 @@
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { client } from "@/sanity/lib/client";
+import { urlForImage } from "@/sanity/lib/image";
+import Image from "next/image";
+import Link from "next/link";
+
+async function getData() {
+  const query = `
+  *[_type == 'blog'] | order(_createdAt desc) {
+    title,
+    smallDescription,
+    "currentSlug": slug.current,
+    titleImage  
+  }`;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+
   return (
     <main>
-      <h1>Hello from the index page</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 mt-10">
+        {data.map((data, idx) => (
+          <Card key={idx}>
+            <Image
+              src={urlForImage(data.titleImage)}
+              alt="image"
+              width={828}
+              height={535}
+              className="object-cover rounded-t-lg"
+            />
+            <CardContent className="mt-5">
+              <h3 className="text-xl font-bold">{data.title}</h3>
+              <p className="line-clamp-3 text-sm mt-2 text-gray-500 dark:text-gray-400">
+                {data.smallDescription}
+              </p>
+              <Button asChild className="w-full mt-7">
+                <Link href={`${data.currentSlug}`}>Read More</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </main>
   );
 }
--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -7,9 +7,11 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-between max-w-2xl mx-auto py-5 px-4 lg:px-0">
       <Link href={"/"} className="font-bold text-3xl">
-        Mark<span className="text-primary">Blog</span>.
+        <h1 className="text-black/70 dark:text-white/80">
+          mark<span className="text-primary/70">slorach</span>.
+        </h1>
       </Link>
-      <ModeToggle/>
+      <ModeToggle />
     </nav>
   );
 };
