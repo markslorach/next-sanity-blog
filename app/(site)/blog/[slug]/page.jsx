a72464b3224280dec9cdf0e01e31e04e79fdf743
Mark Slorach <hello@markslorach.com>
2024-02-28 11:12:21 +0000

    tailwind typography prose classes added to article page

--- app/(site)/blog/[slug]/page.jsx
+++ app/(site)/blog/[slug]/page.jsx
@@ -1,4 +1,9 @@
 import { client } from "@/sanity/lib/client";
+import { urlForImage } from "@/sanity/lib/image";
+import { PortableText } from "@portabletext/react";
+import Image from "next/image";
+
+export const revalidate = 0;
 
 async function getData(slug) {
   const query = `
@@ -18,7 +23,29 @@ const BlogArticle = async ({ params }) => {
   const data = await getData(params.slug);
   console.log(data);
 
-  return <div>{params.slug}</div>;
+  return (
+    <div className="mt-12">
+      <h1>
+        <span className="block text-center text-primary font-semibold tracking-wide uppercase">
+          Mark Slorach - Blog
+        </span>
+        <span className="block text-center mt-2 text-3xl leading-relaxed font-bold tracking-tight sm:text-4xl">
+          {data.title}
+        </span>
+      </h1>
+      <Image
+        src={urlForImage(data.titleImage)}
+        alt="image"
+        width={1920}
+        height={1241}
+        priority
+        className="rounded-lg mt-8 border"
+      />
+      <div className="mt-16 prose prose-blue prose-base dark:prose-invert mb-12 prose-li:marker:text-primary">
+        <PortableText value={data.content} />
+      </div>
+    </div>
+  );
 };
 
 export default BlogArticle;
--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -7,8 +7,8 @@ const NavBar = () => {
   return (
     <nav className="flex items-center justify-between max-w-2xl mx-auto py-5 px-4 lg:px-0">
       <Link href={"/"} className="font-bold text-3xl">
-        <h1 className="text-black/70 dark:text-white/80">
-          mark<span className="text-primary/70">slorach</span>.
+        <h1>
+          mark<span className="text-primary">slorach</span>.
         </h1>
       </Link>
       <ModeToggle />
